e25dc1c49e32cb7c14dd3eadf646abc84355f3ac
Max Humber <max.humber@gmail.com>
2019-08-08 15:56:01 -0400

    scatter is broken....

--- chart/bar.py
+++ chart/bar.py
@@ -9,19 +9,16 @@ def create_label(label, label_width):
 
 # label = create_label('Toronto', label_width=10)
 
-def build_row(value, label, width, mark, display_labels):
+def build_row(value, label, width, mark):
     '''Build a complete row of data'''
     marks = value * mark
     row = marks.ljust(width)
-    if display_labels:
-        row = label + row
+    row = label + row
     return row
 
-# build_row(20, label, mark='🍑', width=20, display_labels=True)
-# build_row(20, label, mark='🍑', width=20, display_labels=False)
+# build_row(20, label, mark='🍑', width=20)
 
-def bar(values, labels, width=30, label_width=None, mark='▇',
-            display_labels=True):
+def bar(values, labels, width=30, label_width=None, mark='▇'):
     '''Create a simple bar chart'''
     if not label_width:
         label_width = max([len(l) for l in labels])
@@ -30,7 +27,7 @@ def bar(values, labels, width=30, label_width=None, mark='▇',
     values = [round(v) for v in values]
     chart = ''
     for value, label in zip(values, labels):
-        row = build_row(value, label, width, mark, display_labels)
+        row = build_row(value, label, width, mark)
         chart += row
         chart += '\n'
     print(chart)
@@ -41,26 +38,5 @@ if __name__ == '__main__':
         'x': ['Toronto', 'Hamilton', 'Carlisle', 'Hong Kong'],
         'y': [3_000_000, 300_000, 3_000, 5_000_000]
     })
-    bar(df.y, df.x, width=20, label_width=10, mark='🙈')
     bar(df.y, df.x)
-    bar(df.y, df.x, display_labels=False, mark='🎑')
-
-# TODO:
-# add fractional marks
-# add data to chart
-
-# DECIMAL_MARKS = {
-#     1: '▏',
-#     2: '▏',
-#     3: '▎',
-#     4: '▍',
-#     5: '▌',
-#     6: '▋',
-#     7: '▊',
-#     8: '▉',
-#     9: '█'
-# }
-#
-# MARK = '▇'
-
-#
+    bar(df.y, df.x, width=20, label_width=10, mark='🙈')
--- chart/scatter.py
+++ chart/scatter.py
@@ -1,27 +1,36 @@
+import random
+from preprocessing import RangeScaler
 
-# ANSI escape SGR Parameters color codes
-AVAILABLE_COLORS = {
-    'red': 91,
-    'blue': 94,
-    'green': 92,
-    'magenta': 95,
-    'yellow': 93,
-    'black': 90,
-    'cyan': 96
-}
+### just dummy data and a quick peek
 
+import numpy as np
+from matplotlib import pyplot as plt
+%matplotlib inline
 
-color = AVAILABLE_COLORS['cyan']
+size = 100
+x = np.random.normal(1000, 500, size)
+y = x * 3 + 200 + np.random.normal(0, 700, size)
 
-r = 15
-c = r * 3
+plt.scatter(x, y)
 
-matrix = [[' '] * c for _ in range(r)]
-chart = ''
-for row in matrix:
-    row_string = ''.join(row)
-    chart += row_string
-    chart += '\n'
+#####
+
+width = 20
+height = 20
+
+matrix = [[' '] * width for _ in range(height)]
+
+wrs = RangeScaler((0, width))
+hrs = RangeScaler((0, height))
+
+x = wrs.fit_transform(x)
+x = [int(round(xi))-1 for xi in x]
+y = wrs.fit_transform(y)
+y = [int(round(yi))-1 for yi in y]
+
+mark = '⋄'
+for (xi, yi) in zip(x, y):
+    matrix[xi][yi] = mark
 
 def matrix_to_string(matrix):
     chart = ''
@@ -31,37 +40,5 @@ def matrix_to_string(matrix):
         chart += '\n'
     return chart
 
-sys.stdout.write(f'\033[{color}m') # Start to write colorized.
-sys.stdout.write(chart)
-sys.stdout.write('\033[0m') # Back to original.
-
-def random_tick():
-    return random.choices([' ', '⋄'], weights=[0.5, 0.5])[0]
-
-from itertools import product
-
-list(product([1,2,3], [1, 2,3]))
-
-matrix = [[' '] * c for _ in range(r)]
-for (x, y) in product(range(r), range(c)):
-    tick = random_tick()
-    print(tick)
-    matrix[x][y] = tick
-
 chart = matrix_to_string(matrix)
-
-sys.stdout.write(f'\033[{color}m') # Start to write colorized.
-sys.stdout.write(chart)
-sys.stdout.write('\033[0m') # Back to original.
-
-size = 100
-x = np.random.normal(1000, 500, size)
-y = x * 3 + 200 + np.random.normal(0, 700, size)
-
-from matplotlib import pyplot as plt
-%matplotlib inline
-
-plt.scatter(x, y)
-
-df = pd.DataFrame({'x': x, 'y': y})
-df
+print(chart)
