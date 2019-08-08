9e37127de7a593c8a93e85b97332d98e62504325
Max Humber <max.humber@gmail.com>
2019-08-08 17:22:01 -0400

    reverse y

--- chart/scatter.py
+++ chart/scatter.py
@@ -1,53 +1,40 @@
 import random
 from preprocessing import RangeScaler
 
-### just dummy data and a quick peek
-
-import numpy as np
-from matplotlib import pyplot as plt
-%matplotlib inline
-
-size = 50
-x = np.random.normal(1000, 500, size)
-y = x * 3 + 200 + np.random.normal(0, 700, size)
-
-plt.scatter(x, y)
-
-#####
-
-width = 30
-height = 10
-
-matrix = [[' '] * width for _ in range(height)]
-
-wrs = RangeScaler((0, width-1))
-hrs = RangeScaler((0, height-1))
-
-x = wrs.fit_transform(x)
-x = [int(round(xi)) for xi in x]
-y = hrs.fit_transform(y)
-y = [int(round(yi)) for yi in y]
-
-list(zip(x, y))
-max(x)
-max(y)
-
-mark = '🍑'
-for (xi, yi) in zip(x, y):
-    matrix[yi][xi] = mark
-
-matrix = [row[::-1] for row in matrix]
-
-xi
-yi
+def scale_values(values, scale):
+    values = RangeScaler((0, scale-1)).fit_transform(values)
+    values = [int(round(v)) for v in values]
+    return values
 
 def matrix_to_string(matrix):
-    chart = ''
+    string = ''
     for row in matrix:
         row_string = ''.join(row)
-        chart += row_string
-        chart += '\n'
-    return chart
+        string += row_string
+        string += '\n'
+    return string
 
-chart = matrix_to_string(matrix)
-print(chart)
+def scatter(x, y, width=40, height=None, mark='•'):
+    if not height:
+        height = int(width / 3 // 1)
+    matrix = [[' '] * width for _ in range(height)]
+    x = scale_values(x, width)
+    y = scale_values(y, height)
+    for (xi, yi) in zip(x, y):
+        matrix[yi][xi] = mark
+    matrix = matrix[::-1]
+    chart = matrix_to_string(matrix)
+    print(chart)
+
+if __name__ == '__main__':
+    import numpy as np
+    from matplotlib import pyplot as plt
+    %matplotlib inline
+
+    size = 50
+    x = np.random.normal(1000, 500, size)
+    y = x * 3 + 200 + np.random.normal(0, 700, size)
+
+    plt.scatter(x, y)
+
+    scatter(x, y, mark='🐞')
