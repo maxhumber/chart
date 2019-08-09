5193a1c1d8fbe85f82249a265e5e57f151e7ee95
Max Humber <max.humber@gmail.com>
2019-08-09 14:45:52 -0400

    two more tests

--- tests/test_chart.py
+++ tests/test_chart.py
@@ -1,4 +1,5 @@
 from chart import RangeScaler
+from chart import bar, scatter
 
 def test_range_scaler():
     rs = RangeScaler(out_range=(0, 10), floor=0, round=True)
@@ -7,25 +8,23 @@ def test_range_scaler():
     result = rs.transform(x)
     assert result == [3, 5, 10, 9, 8, 4]
 
-# import random
-#
-# import chart
-# from chart import RangeScaler
-#
-# random.seed(1993)
-# x = [random.gauss(10, 5) for _ in range(50)]
-# y = [xi * 2 + 5 for xi in x]
-#
-# chart.scatter(x, y, width=25)
-# chart.scatter(x, y, width=25, mark='🍑')
-#
-# labels = ['Toronto', 'Hamilton', 'Carlisle', 'Hong Kong']
-# values = [3_000_000, 300_000, 3_000, 5_000_000]
-#
-# chart.bar(values, labels)
-# chart.bar(values, labels, width=10, label_width=3, mark='🍑')
-#
-# x = [40, 25, 80, 60, 100]
-# RangeScaler((0, 100)).fit_transform(x)
-# RangeScaler((0, 100), 0).fit_transform(x)
-# RangeScaler((0, 50), 0).fit_transform(x)
+def test_scatter():
+    # redirect sys.stdout to a buffer
+    import sys, io
+    stdout = sys.stdout
+    sys.stdout = io.StringIO()
+    # call scatter which will print to console
+    scatter(range(0, 20), range(0, 20), width=15)
+    # capture that output and restore sys.stdout
+    output = sys.stdout.getvalue()
+    sys.stdout = stdout
+    assert output == '             ••\n         ••••  \n      •••      \n  ••••         \n••             \n\n'
+
+def test_bar():
+    import sys, io
+    stdout = sys.stdout
+    sys.stdout = io.StringIO()
+    bar([300, 400, 200, 80, 500], ['A', 'B', 'C', 'D', 'E'])
+    output = sys.stdout.getvalue()
+    sys.stdout = stdout
+    assert output == 'A: ▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇            \nB: ▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇      \nC: ▇▇▇▇▇▇▇▇▇▇▇▇                  \nD: ▇▇▇▇▇                         \nE: ▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇\n\n'
