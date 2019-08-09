dcb4d0342026126a36ce11dbdfa407f933434098
Max Humber <max.humber@gmail.com>
2019-08-09 11:34:29 -0400

    env var pass

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name='chart',
-    version='0.1.1',
+    version='0.1.2',
     description='chart',
     long_description=long_description,
     long_description_content_type="text/markdown",
